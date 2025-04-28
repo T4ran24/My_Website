c1f3aaa9599d689ce52567caf31f26c8441a2b94
Madina <teymurovamadina04@gmail.com>
2025-04-28 07:04:37 -0700

    Create login.js

---
+++ Website_Main3/login.js
@@ -0,0 +1,34 @@
+function toggleLogin() {
+     const dropdown = document.querySelector('.login-dropdown');
+     
+     if (!dropdown) {
+         createLoginDropdown();
+     } else {
+         dropdown.classList.toggle('active');
+     }
+     
+     document.addEventListener('click', function(event) {
+         const isClickInside = event.target.closest('.user-account') || 
+                              event.target.closest('.login-dropdown');
+         
+         if (!isClickInside) {
+             const dropdown = document.querySelector('.login-dropdown');
+             if (dropdown) dropdown.classList.remove('active');
+         }
+     });
+ }
+ 
+ function createLoginDropdown() {
+     const dropdownHTML = `
+         <div class="login-dropdown">
+             <h3>Sign in</h3>
+             <input type="text" placeholder="Email or phone number">
+             <input type="password" placeholder="password">
+             <button>sign in</button>
+             <p>don't have an account <a href="#">Sign up</a></p>
+         </div>
+     `;
+     
+     const header = document.querySelector('.main-header');
+     header.insertAdjacentHTML('beforeend', dropdownHTML);
+ }
